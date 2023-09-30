a4e8b18f1a8ebed8afe4c717b0eae31ce0d29c30
Artem Sibisov <artsib7777@yandex.ru>
2023-10-01 01:27:35 +0800

    поменять BrawserRouter на HashRouter для правильного деплоя

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App'; //входная точка всех компонентов
 
@@ -8,8 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( //рендер входной точки (App)
   //StrictMode - инструмент  для обнаружения ошибок, проблем в приложении
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
